2d45fbfd870445e4f090501fff5d3e5b40f1bd20
admin <admin@10.137.138.152>
2018-01-02 16:49:22 +0900

    Chapter02

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,11 @@
+package icehs.science.chapter02;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world~~");
+		System.out.println("동해물과 백두산이 마르고 닳도록");
+		
+	}
+}
