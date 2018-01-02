f96f040a0724a2be06801acb31aba3e5c29a3fe0
admin <admin@10.137.138.152>
2018-01-02 17:05:52 +0900

    ㅎㅇㄹ

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -2,10 +2,11 @@ package icehs.science.chapter02;
 
 public class HelloWorld
 {
+	//나의 첫번째 자바 프로그램
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world~~");
 		System.out.println("동해물과 백두산이 마르고 닳도록");
-		
+		//System.out.println("Hi");
 	}
 }
