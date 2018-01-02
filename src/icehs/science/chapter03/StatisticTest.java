aaa3d969bb79028bfd077b39140d6c3a965d2434
admin <admin@10.137.138.152>
2018-01-02 18:14:02 +0900

    Can I go home?

---
+++ src/icehs/science/chapter03/StatisticTest.java
@@ -0,0 +1,19 @@
+package icehs.science.chapter03;
+
+public class StatisticTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int distance = 15000000;
+		double lottoProbability = 0.0000001235;
+		long population = 6973738433L;
+		boolean isRight = true;
+		
+		System.out.println("지구에서 태양까지의 거리 : " + distance);
+		System.out.println("로또에 당쳠될 확륙 : " + lottoProbability);
+		System.out.println("전 세계의 인구 수 : " + population);
+		System.out.println("위 값들이 정확한가요? : " + isRight);
+	}
+
+}
---
+++ src/icehs/science/chapter03/Variables.java
@@ -0,0 +1,16 @@
+package icehs.science.chapter03;
+
+public class Variables
+{
+	public static void main(String[] Args)
+	{
+		int age = 19;
+		int koreanScore = 95;
+		int englishScore = 100;
+		int mathScore = 88;
+		int totalGoodsPrice = 111111;
+		System.out.println(koreanScore);
+		System.out.println(englishScore);
+		System.out.println(mathScore);
+	}
+}
