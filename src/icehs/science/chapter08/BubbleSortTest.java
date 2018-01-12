fe8b911bf2663149f81fb5f115927b803ea4b644
admin <admin@10.137.138.152>
2018-01-12 15:57:23 +0900

    비교 메서드를 따로 만듬

--- src/icehs/science/chapter08/BubbleSortTest.java
+++ src/icehs/science/chapter08/BubbleSortTest.java
@@ -1,10 +1,10 @@
-///////////////
-//오름차순으로 정렬//
-///////////////
+//////////////////
+//오름차순으로 배열 정렬//
+//////////////////
 
 package icehs.science.chapter08;
 
-
+//버블정렬이 아닐수도 있음
 public class BubbleSortTest {
 	public static void main(String[] args) {
 		int[] numbers = {2, 5, 1, 3, 4, 6, 8};
@@ -12,7 +12,7 @@ public class BubbleSortTest {
 		for (int i = 0; i < numbers.length; i++) {
 			for (int j = i; j > 0; j--) {
 				//왼쪽 요소가 오른쪽 요소보다 크면 서로 교환
-				if(numbers[j - 1] > numbers[j]) {
+				if(Compare(numbers[j - 1], numbers[j])) {
 					int temp = numbers[j - 1];
 					numbers[j - 1] = numbers[j];
 					numbers[j] = temp;
@@ -27,6 +27,16 @@ public class BubbleSortTest {
 		//결과 출력
 		for (int i = 0; i < numbers.length; i++) {
 			System.out.print(numbers[i] + " ");
+		}
+	}
+	
+	static boolean Compare(int left, int right) {
+		//내림차순은 if(left < right)로 바꾸면 됨
+		if(left > right) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 }
